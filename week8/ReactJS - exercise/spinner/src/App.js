425664e1ca3a3cd21f43a66369029dcfe6db1e0c
Roee Angel <roee.angel93@gmail.com>
2020-09-15 15:10:17 +0300

    add new task

---
+++ week8/ReactJS - exercise/spinner/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+import Spinner from "./Spinner";
+
+class App extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        {this.state.isLoading ? <Spinner /> : <h1>TADAAAAAAAAA </h1>}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week8/ReactJS - exercise/spinner/src/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import spinner from "./spinnergif.gif";
+import "./Spinner.css";
+export default function Spinner() {
+  return (
+    <div>
+      <div class="loadingio-spinner-rolling-3mzv2jqvk7b">
+        <div class="ldio-vm1jipnib7">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
