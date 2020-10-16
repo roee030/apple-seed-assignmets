a39df04e3f60878d092e6dd7e409b8b8c1d39bd3
Roee Angel <roee.angel93@gmail.com>
2020-10-16 16:11:21 +0300

    add express Router

--- week11/nodeJS - playground/index.js
+++ week11/nodeJS - playground/index.js
@@ -1,6 +1,8 @@
 const { response } = require('express');
 const express = require('express')
 const path = require('path')
+const logger = require('./middleware/logger')
+const members = require('./Members')
 const app = express();
 
 const PORT = process.env.PORT || 5000
@@ -9,3 +11,5 @@ app.listen(PORT, ()=>{
 })
 //Set public folder
 app.use(express.static(path.join(__dirname,'public')))
+//Get all Members 
+app.use('/api/members',require('./routes/api/members'))
---
+++ week11/nodeJS - playground/routes/api/members.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const members = requrie('../../Members')
+
+const idFilter = req => member => member.id === parseInt(req.params.id);
+
+
+// Gets All Members
+router.get('/', (req, res) => res.json(members));
+
+// Get Single Member
+router.get('/:id', (req, res) => {
+  const found = members.some(idFilter(req));
+
+  if (found) {
+    res.json(members.filter(idFilter(req)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
