a2ac1376cac22072a2866ad862722068d7bd9a14
Roee Angel <roee.angel93@gmail.com>
2020-10-16 16:23:44 +0300

    add get methods for members

--- week11/nodeJS - playground/routes/api/members.js
+++ week11/nodeJS - playground/routes/api/members.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const members = requrie('../../Members')
+const members = require('../../Members')
 
 const idFilter = req => member => member.id === parseInt(req.params.id);
 
@@ -18,3 +18,9 @@ router.get('/:id', (req, res) => {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
   }
 });
+
+//Create Member 
+router.post('/',(req,res)=>{
+    res.send(req.body)
+})
+module.exports = router;
